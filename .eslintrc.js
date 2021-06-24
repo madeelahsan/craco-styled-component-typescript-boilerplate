cee2aa0e444a6158ad8dd5db813209f650ed2913
M Adeel Ahsan <madelahsan@gmail.com>
2021-06-24 18:38:41 +0500

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'react/prop-types': 'off',
+        'no-console': 'warn',
+        '@typescript-eslint/no-explicit-any': 0,
+        'no-restricted-imports': [
+            'error',
+            {
+                paths: [
+                    {
+                        name: 'styled-components',
+                        message: 'Please import from styled-components/macro.',
+                    },
+                ],
+                patterns: ['!styled-components/macro'],
+            },
+        ],
+    },
+};
